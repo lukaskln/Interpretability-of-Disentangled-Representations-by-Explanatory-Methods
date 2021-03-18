c7f33a5688c013cd3498ef6676c1ba089ec8f6bd
lukaskln <70696369+lukaskln@users.noreply.github.com>
2021-03-18 16:04:10 +0100

    New CNN enc/dec

--- src/models/encoder/VAE_loss/betaTCVAE_CNN.py
+++ src/models/encoder/VAE_loss/betaTCVAE_CNN.py
@@ -26,26 +26,29 @@ class betaTCVAE_CNN(pl.LightningModule):
 
         # Encoder
 
-        self.enc_conv1 = nn.Conv2d(
-            in_channels=1, out_channels=c, kernel_size=4, stride=2, padding=1)  # out: c x 14 x 14
-        self.enc_conv2 = nn.Conv2d(
-            in_channels=c, out_channels=c*2, kernel_size=4, stride=2, padding=1)  # out: c x 7 x 7
-        self.fc_mu = nn.Linear(in_features=c*2*7*7, out_features=latent_dim)
-        self.fc_logvar = nn.Linear(
-            in_features=c*2*7*7, out_features=latent_dim)
+        self.enc_conv1 = nn.Conv2d(in_channels=1, out_channels=c,kernel_size=5, stride=1, padding=1)  # out: c x 14 x 14
+        self.enc_pool1 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.enc_conv2 = nn.Conv2d(in_channels=c, out_channels=c*2,kernel_size=5, stride=1, padding=1)  # out: c x 7 x 7
+        self.enc_pool2 = nn.MaxPool2d(2, stride=2, padding=0)
+        self.enc_fc = nn.Linear(in_features=c*50, out_features=10*c)
+        self.fc_mu = nn.Linear(in_features=10*c, out_features=latent_dim)
+        self.fc_logvar = nn.Linear(in_features=10*c, out_features=latent_dim)
 
         # Decoder
-
+        self.dec_bn1 = nn.BatchNorm1d(latent_dim)
         self.fc = nn.Linear(in_features=latent_dim, out_features=c*2*7*7)
-        self.dec_conv2 = nn.ConvTranspose2d(
-            in_channels=c*2, out_channels=c, kernel_size=4, stride=2, padding=1)
-        self.dec_conv1 = nn.ConvTranspose2d(
-            in_channels=c, out_channels=1, kernel_size=4, stride=2, padding=1)
+        self.dec_bn2 = nn.BatchNorm1d(c*2*7*7)
+        self.dec_conv2 = nn.ConvTranspose2d(in_channels=c*2, out_channels=c, kernel_size=4, stride=2, padding=1)
+        self.dec_bn3 = nn.BatchNorm2d(c)
+        self.dec_conv1 = nn.ConvTranspose2d(in_channels=c, out_channels=1, kernel_size=4, stride=2, padding=1)
 
     def encode(self, x):
         x = F.relu(self.enc_conv1(x))
+        x = self.enc_pool1(x)
         x = F.relu(self.enc_conv2(x))
+        x = self.enc_pool2(x)
         x = x.view(x.size(0), -1)
+        x = F.relu(self.enc_fc(x))
         mu = self.fc_mu(x)
         log_var = self.fc_logvar(x)
         return mu, log_var
@@ -57,9 +60,12 @@ class betaTCVAE_CNN(pl.LightningModule):
         return z
 
     def decode(self, z):
-        x = self.fc(z)
+        x = self.dec_bn1(z)
+        x = self.fc(x)
+        x = self.dec_bn2(x)
         x = x.view(x.size(0), self.c*2, 7, 7)
         x = F.relu(self.dec_conv2(x))
+        x = self.dec_bn3(x)
         x = self.dec_conv1(x)
         return torch.sigmoid(x)
 
