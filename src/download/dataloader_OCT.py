91812618eed6b2be5b43e5a3b7e0751f359acfc3
lukaskln <70696369+lukaskln@users.noreply.github.com>
2021-05-03 12:16:30 +0200

    multi GPU changes

--- src/download/dataloader_OCT.py
+++ src/download/dataloader_OCT.py
@@ -8,12 +8,11 @@ import zipfile
 import numpy as np
 import urllib.request as request
 
-from torch.utils.data import DataLoader, random_split, TensorDataset
+from torch.utils.data import DataLoader, random_split, TensorDataset, DistributedSampler
 from torch import Tensor
 import torch
 import torchvision
 from torchvision import transforms
-
 
 def make_weights_for_balanced_classes(images, nclasses):
     count = [0] * nclasses
@@ -27,6 +26,7 @@ def make_weights_for_balanced_classes(images, nclasses):
     for idx, val in enumerate(images):
         weight[idx] = weight_per_class[val[1]]
     return weight
+
 
 class OCT_DataModule(pl.LightningDataModule):
     def __init__(self, batch_size, data_dir, seed):
@@ -53,15 +53,21 @@ class OCT_DataModule(pl.LightningDataModule):
         self.train_enc, self.val_enc = random_split(data_enc, [85600, 21400],  
                                     generator=torch.Generator().manual_seed(self.seed))
 
-        weights = make_weights_for_balanced_classes(train.imgs, len(train.classes))
-        weights = torch.DoubleTensor(weights)
-        weights_enc, weights_cla = random_split(weights, [107000, 1309],  # 108309
-                            generator=torch.Generator().manual_seed(self.seed))
-        weights_enc, _ = random_split(weights_enc, [85600, 21400],
-                                    generator=torch.Generator().manual_seed(self.seed))
+        if torch.cuda.device_count() > 1:
+            self.sampler_enc = DistributedSampler(self.train_enc)
+            self.sampler_cla = DistributedSampler(self.train_cla)
 
-        self.sampler_enc = torch.utils.data.sampler.WeightedRandomSampler(weights_enc, len(weights_enc))
-        self.sampler_cla = torch.utils.data.sampler.WeightedRandomSampler(weights_cla, len(weights_cla))
+        else:
+            weights = make_weights_for_balanced_classes(train.imgs, len(train.classes))
+            weights = torch.DoubleTensor(weights)
+            weights_enc, weights_cla = random_split(weights, [107000, 1309],  # 108309
+                                generator=torch.Generator().manual_seed(self.seed))
+            weights_enc, _ = random_split(weights_enc, [85600, 21400],
+                                        generator=torch.Generator().manual_seed(self.seed))
+
+            self.sampler_enc = torch.utils.data.sampler.WeightedRandomSampler(weights_enc, len(weights_enc))
+
+            self.sampler_cla = torch.utils.data.sampler.WeightedRandomSampler(weights_cla, len(weights_cla))
 
         self.test = torchvision.datasets.ImageFolder(self.data_dir + "/test", transform=transform_img)
 
