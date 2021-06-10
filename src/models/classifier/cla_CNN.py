2c169722d4ccce38837047846ad8192698ef6a00
lukaskln <70696369+lukaskln@users.noreply.github.com>
2021-06-10 16:52:42 +0200

    LR of cla

--- src/models/classifier/cla_CNN.py
+++ src/models/classifier/cla_CNN.py
@@ -18,7 +18,7 @@ class CNN(pl.LightningModule):
         input_dim: int,
         num_classes: int,
         bias: bool = True,
-        learning_rate: float = 0.0001,
+        learning_rate: float = 0.00001,
         optimizer: Optimizer = Adam,
         **kwargs
     ):
